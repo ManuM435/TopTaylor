fe3183fcf5961e4da91aed3f3ce6084c9e701113
Manuel Meiriño <mmeirino@udesa.edu.ar>
2024-05-26 20:09:32 -0300

    making Big6 play work

--- javascript/bestSixes.js
+++ javascript/bestSixes.js
@@ -2,7 +2,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const categories = document.querySelectorAll('.category');
 
     categories.forEach(category => {
-        category.addEventListener('click', () => {
+        category.addEventListener('click', (event) => {
+            // Prevent the default action of hiding the song list when clicking a song item
+            if (event.target.closest('.song-item')) return;
+
             const songList = category.querySelector('.song-list');
             if (songList.style.display === 'block') {
                 songList.style.display = 'none';
@@ -17,22 +20,35 @@ document.addEventListener('DOMContentLoaded', () => {
     playButtons.forEach(button => {
         button.addEventListener('click', () => {
             const audio = button.nextElementSibling;
+            const icon = button.querySelector('i');
+            
             if (audio.paused) {
                 // Pause all other audio elements
                 document.querySelectorAll('.audio-player').forEach(player => {
                     if (player !== audio) {
                         player.pause();
+                        player.currentTime = 0; // Reset audio
+                        const playButton = player.previousElementSibling;
+                        playButton.querySelector('i').classList.remove('fa-pause');
+                        playButton.querySelector('i').classList.add('fa-play');
                     }
                 });
+
                 audio.play();
-                button.textContent = 'Pause';
-            } else {
+                audio.paused = false;
+                icon.classList.remove('fa-play');
+                icon.classList.add('fa-pause');
+
+            } 
+            else {
                 audio.pause();
-                button.textContent = 'Play';
+                icon.classList.remove('fa-pause');
+                icon.classList.add('fa-play');
             }
 
             audio.addEventListener('ended', () => {
-                button.textContent = 'Play';
+                icon.classList.remove('fa-pause');
+                icon.classList.add('fa-play');
             });
         });
     });
